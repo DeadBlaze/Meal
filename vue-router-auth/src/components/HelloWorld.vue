078698b2c2bfd20d4958e62d9a67da1898449322
payfoxxx <57244868+payfoxxx@users.noreply.github.com>
2021-07-02 17:16:20 +0700

    Add files via upload

---
+++ vue-router-auth/src/components/HelloWorld.vue
@@ -0,0 +1,57 @@
+<template>
+<div class="hello">
+    <h1>Добро пожаловать на наш сайт!</h1>
+    <h2>Наш вариант: BAUART Meal Техническое задание (Альтернативное v.1 Node.js)</h2>
+    <button type="submit" @click="handleSubmit1">
+        Login
+    </button>
+    <button type="submit" @click="handleSubmit2">
+        Register
+    </button>
+    <button type="submit" @click="handleSubmit3">
+        Logout
+    </button>
+</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+        }
+    },
+    methods: {
+        handleSubmit1() {
+            this.$router.push('login')
+        },
+        handleSubmit2() {
+            this.$router.push('register')
+        },
+        handleSubmit3() {
+            localStorage.clear()
+        }
+    }
+}
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
+<style scoped>
+h2 {
+    font-weight: normal;
+    margin-bottom: 50px
+}
+
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+
+li {
+    display: inline-block;
+    margin: 0 10px;
+}
+
+a {
+    color: #42b983;
+}
+</style>
