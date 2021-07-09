a8fd20c39ae0f5f7eda0ae2f59988c9a6d1512fc
payfoxxx <57244868+payfoxxx@users.noreply.github.com>
2021-07-09 14:10:05 +0700

    Add files via upload

---
+++ vue-router-auth/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <ul id="ulc">
+    <li class="lic"><router-link to="/" exact active-class="active">Главная страница</router-link></li>
+    <li class="lic"><router-link to="/dashboard" active-class="active">Меню</router-link></li>
+    <li v-if="is_admin==1" class="lic"><router-link to="/admin" active-class="active">Админ</router-link></li>
+    </ul>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+let user = JSON.parse(localStorage.getItem('user'));
+export default {
+  name: 'App',
+  data(){
+    return{
+      is_admin: user.is_admin
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#ulc {
+  list-style-type: none;
+  list-style-position: inside;
+  background-color: rgb(176, 255, 215);
+  height: 30px;
+}
+.lic{
+  display: inline-block;
+  margin-top: 5px;
+  margin-right: 50px;
+}
+</style>
